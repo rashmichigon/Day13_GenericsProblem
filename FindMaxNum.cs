ae6f69de37a206519bb84d0fa94ab3e0ffb19b90
Rashmi <rashmichigon65@gmail.com>
2022-12-22 17:59:51 +0530

    Day13_GenericsProblem

---
+++ FindMaxNum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13_GenericsProgram
+{
+    internal class FindMaxNum
+    {
+        public static void MaxValue(int first, int second, int third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                Console.WriteLine("{0} is Maximum", first);
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                Console.WriteLine("{0} is  Maximum", second);
+            }
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                Console.WriteLine("{0} is Maximum", third);
+            }
+            else
+                Console.WriteLine("All The Numbers Are Equal");
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Day13_GenericsProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Generics Problem");
+            FindMaxNum.MaxValue(30, 30, 30);
+        }
+    }
+}
