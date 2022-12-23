00b292a43f71ab56234da2ed8b323f8c7b64db4f
Rashmi <rashmichigon65@gmail.com>
2022-12-23 20:53:40 +0530

    UC5-ExtendthemaxMethod

--- Program.cs
+++ Program.cs
@@ -1,17 +1,24 @@
-﻿namespace Day13_GenericsProgram
-{
-    internal class Program
-    {
+﻿using System.IO;
+using System;
+namespace Day13_GenericsProgram
+ {
+        internal class Program
+        {
         static void Main(string[] args)
         {
-            int[] array = { 4, 3, 2, 1 };
             Console.WriteLine("Welcome To Generics Problem");
+            //UC1
             FindMaxNum.MaxValue(20, 50, 30);
+            //UC2
             FindMaxNum.MaxValue(90.22f, 20.111f, 10.2f);
+            //UC3
             FindMaxNum.MaxValue("Apple", "Peach", "Banana");
-
+            //UC4
             GenericsFindMax<int> genericsFindMax = new GenericsFindMax<int>();
+            int[] array = { 4, 3, 2, 1 };
             genericsFindMax.MaxVal(array);
+            //UC5
+            GenericsFindMax<int>.FindMaxValue(1, 2, 3);
         }
     }
 }
