1165d536a1414bb2270e089e30c6ec98ff684ad9
Rashmi <rashmichigon65@gmail.com>
2022-12-24 11:06:59 +0530

    Refactor1

--- Program.cs
+++ Program.cs
@@ -4,14 +4,11 @@
     {
         static void Main(string[] args)
         {
-            int[] array = { 4, 3, 2, 1 };
             Console.WriteLine("Welcome To Generics Problem");
             FindMaxNum.MaxValue(20, 50, 30);
             FindMaxNum.MaxValue(90.22f, 20.111f, 10.2f);
             FindMaxNum.MaxValue("Apple", "Peach", "Banana");
-
-            GenericsFindMax<int> genericsFindMax = new GenericsFindMax<int>();
-            genericsFindMax.MaxVal(array);
+            Console.ReadLine();
         }
     }
 }
